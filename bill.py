aaaa3189fb221b7237919daa0e859b52b648aac1
Naresh <ff.jonnala@gmail.com>
2019-02-12 11:52:22 +0530

    assignment

---
+++ bill.py
@@ -0,0 +1,10 @@
+company = "vermaclabs"
+address = "Madhapur, hyderabad"
+date = "12/02/2019"
+time = "11:49"
+staff = "Deelip"
+
+print("vermaclabs")
+print("Madhapur, hyderabad")
+print("date: 12/02/2019 11:49")
+print("Saff: Deelip")
