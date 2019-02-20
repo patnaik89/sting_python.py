dd215d0b98f92daf1f923eb43e577f0ff1cf8d3f
patnaik89 <47376479+patnaik89@users.noreply.github.com>
2019-02-20 11:37:40 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,17 +1,19 @@
 list1 = ['physics', 'chemistry', 1997, 2000];
 list2 = [1, 2, 3, 4, 5 ];
-list3 = ["a", "b", "c", "d"]
+list3 = ["a", "b", "c", "d"] 
 
 list1 = ['physics','chemistry',2000,2003];
 list2 = [1,2,3,5 ];
 list3 = ["a", "b", "c", "d"]
 
+# accessing value in list 
 #!/usr/bin/python
 list1 = ['physics', 'chemistry', 2000,2003];
 list2 = [1,2,3,4,5,6,7]
 print("list1[0]:", list1[0])
 print ("list2[1:5]:", list2[1:5])
 
+# updating list
 #!/usr/bin/python
 list = ['physics', 'chemistry', 2000, 2003];
 print("value available at index 2 :")
