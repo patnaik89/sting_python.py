5a6fd6cae32b0405b0b339a339693f849f5e3a22
patnaik89 <47376479+patnaik89@users.noreply.github.com>
2019-02-20 11:38:52 +0530

    Update list.py

--- list.py
+++ list.py
@@ -3,7 +3,7 @@ list2 = [1, 2, 3, 4, 5 ];
 list3 = ["a", "b", "c", "d"] 
 
 list1 = ['physics','chemistry',2000,2003];
-list2 = [1,2,3,5 ];
+list2 = [1,2,3,4,5 ];
 list3 = ["a", "b", "c", "d"]
 
 # accessing value in list 
