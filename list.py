10776a80bc25f769664b95b72419542da015227d
patnaik89 <47376479+patnaik89@users.noreply.github.com>
2019-02-20 11:23:17 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,11 @@
+list1 = ['physics', 'chemistry', 1997, 2000];
+list2 = [1, 2, 3, 4, 5 ];
+list3 = ["a", "b", "c", "d"]
+
+list1 = ['physics','chemistry',2000,2003];
+list2 = [1,2,3,5 ];
+list3 = ["a", "b", "c", "d"]
+
+#!/usr/bin/python
+list1 = ['physics', 'chemistry', 2000,2003];
+list2 = [1,2,3,4,5,6,7]
