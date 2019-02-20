135a4b73c283bcacdf6cdb1984d9744da9218404
patnaik89 <47376479+patnaik89@users.noreply.github.com>
2019-02-20 11:30:56 +0530

    Update list.py

--- list.py
+++ list.py
@@ -9,3 +9,9 @@ list3 = ["a", "b", "c", "d"]
 #!/usr/bin/python
 list1 = ['physics', 'chemistry', 2000,2003];
 list2 = [1,2,3,4,5,6,7]
+print("list1[0]:", list1[0])
+print ("list2[1:5]:", list2[1:5])
+
+#!/usr/bin/python
+list = ['physics', 'chemistry', 2000, 2003];
+print("value available at index 2 :")
