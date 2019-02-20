5246959bb38fcd1ffdc35e8664eabdb403ff7b13
patnaik89 <47376479+patnaik89@users.noreply.github.com>
2019-02-20 11:39:27 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,7 +1,3 @@
-list1 = ['physics', 'chemistry', 1997, 2000];
-list2 = [1, 2, 3, 4, 5 ];
-list3 = ["a", "b", "c", "d"] 
-
 list1 = ['physics','chemistry',2000,2003];
 list2 = [1,2,3,4,5 ];
 list3 = ["a", "b", "c", "d"]
