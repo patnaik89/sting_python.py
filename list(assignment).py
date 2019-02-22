bd5f1df6dc40a55d1adf9f0aaaaf11c54adfbde3
patnaik89 <47376479+patnaik89@users.noreply.github.com>
2019-02-22 12:04:24 +0530

    Create list(assignment).py

---
+++ list(assignment).py
@@ -0,0 +1,11 @@
+#create list
+fruits = ['apple', 'banana', 'cherry']
+fruit1 = ['Pine apple', 'Graps', 'Orange']
+
+# extend list
+fruits.extend(fruit1)
+print(fruits)
+
+# sort list
+fruit1.sort()
+print(fruit1)
